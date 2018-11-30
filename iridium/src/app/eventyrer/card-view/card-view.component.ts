8e7abade6eb1293d140e37fbc32a7cd4e4077df5
EVogel1999 <eav990@gmail.com>
2018-11-30 15:44:06 -0500

    Implemented bare bones content view page

--- iridium/src/app/eventyrer/card-view/card-view.component.ts
+++ iridium/src/app/eventyrer/card-view/card-view.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { CampaignService } from '../../services/campaign.service';
+import { Campaign } from 'src/app/interfaces/campaign';
 
 @Component({
   selector: 'app-card-view',
@@ -8,14 +9,28 @@ import { CampaignService } from '../../services/campaign.service';
   styleUrls: ['./card-view.component.css']
 })
 export class CardViewComponent implements OnInit {
-  campaign: any;
+  campaign: Campaign = {
+    id: '',
+    name: '',
+    author: '',
+    date: '',
+    description: ''
+  };;
 
   constructor(private route: ActivatedRoute, private router: Router, private service: CampaignService) { }
 
   ngOnInit() {
+    let data: Campaign;
     this.service.getCampaign(this.route.snapshot.params.id).subscribe(val => {
-      this.campaign = val;
+      data = val;
     });
+    if (data === undefined) {
+      location.reload();
+    }
+    else {
+      this.campaign = data;
+      console.log(this.campaign);
+    }
   }
 
 }
